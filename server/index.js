7ebfd59dae6c2379f82bdfe06584d4d4288af250
thangld <thangld2@rikkeisoft.com>
2022-06-08 12:06:57 +0700

    set up MERN stack project - build first schema

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const port = process.env.PORT || 5000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
